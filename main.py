2bc44698fde2042b25761714701f888d90ceb318
ahmad <mr.a.gamal@gmail.com>
2026-04-04 20:03:28 +0200

    fix ui finally

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-@app.get("/ui")
-def ui():
-    return {"status": "ui route works"}
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="template")
+
+@app.get("/")
+def root():
+    return {"message": "Ultra Power Solutions System Running"}
+
+@app.get("/ui", response_class=HTMLResponse)
+def ui(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
